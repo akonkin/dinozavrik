a40c4abf9aeec83edbd2c3742a14babef31755d1
Alexander Konkin <a.a.konkin@gmail.com>
2019-06-23 23:07:57 +0200

    cleaned the mess a bit

--- dino.py
+++ dino.py
@@ -12,19 +12,20 @@ import tensorflow as tf
 
 
 class Pac:
-    def __init__(self, game_over, environ):
+    def __init__(self, game_over, environ, env_len=32):
         self.alive = True
         self.speed = 0
-        self.mem = [[0]*64+[0]]
+        self.mem = [([0] * env_len * 2, 0)]
         self.X = []
         self.y = []
         self.w = []
+        self.env_len = env_len
         self.jump_time = 0
         self.first_game = True
 
         self.model = Sequential()
-        self.model.add(Dense(64, input_dim=64, activation="relu", kernel_initializer="he_uniform"))
-        self.model.add(Dense(64, activation="relu", kernel_initializer="he_uniform"))
+        self.model.add(Dense(env_len*2, input_dim=64, activation="relu", kernel_initializer="he_uniform"))
+        self.model.add(Dense(env_len*2, activation="relu", kernel_initializer="he_uniform"))
         self.model.add(Dense(1))
         self.model.add(Activation("sigmoid"))
         self.model.compile(loss="binary_crossentropy", optimizer='adam',
@@ -42,52 +43,45 @@ class Pac:
         self.jump_time = time.time()
 
     def think(self):
-        # Переписать
-        if len(self.mem) > 3:
-            for row in self.mem[:-3]:
-                self.X.append(row[:-1])
-                self.y.append([row[-1]])
-                self.w.append(1)
-                print(''.join((str(l) for l in self.X[-1])), self.y[-1])
-            print('')
-            for row in self.mem[-3:]:
-                self.X.append(row[:-1])
-                self.y.append([int(not row[-1])])
-                self.w.append(0.5)
-                print(''.join((str(l) for l in self.X[-1])), self.y[-1])
+        mem_len = len(self.mem)
+        for i in range(mem_len):
+            if i < mem_len - 3:
+                y_ = self.mem[i][1]
+                w_ = 1
+            else:
+                y_ = int(not self.mem[i][1])
+                w_ = 0.5
 
-        else:
-            print('low')
-            for row in self.mem:
-                self.X.append(row[:-1])
-                self.y.append([int(not row[-1])])
-                self.w.append(0.5)
-                print(''.join((str(l) for l in self.X[-1])), self.y[-1])
+            self.X.append(self.mem[i][0])
+            self.y.append([y_])
+            self.w.append(w_)
+
+            print(''.join((str(l) for l in self.X[-1])), self.y[-1], self.w[-1])
 
         with open('data_2.sav', 'w+') as d_file:
             for i in range(len(self.X)):
                 d_file.write('{},{},{}\n'.format(self.X[i], self.y[i], self.w[i]))
 
-        self.mem = [[0]*64+[0]]
+        self.mem = [([0] * self.env_len * 2, 0)]
 
         print('X:', np.shape(self.X), 'Classes:', len(np.unique(self.y)))
 
         if len(np.unique(self.y)) > 1:
 
             if np.shape(self.X)[0] < 100:
-                MAX_EPOCHS = 10
+                epochs = 10
             else:
-                MAX_EPOCHS = 1
+                epochs = 1
 
             with self.graph.as_default():
-                self.model.fit(np.array(self.X), np.array(self.y), sample_weight=np.array(self.w), epochs=MAX_EPOCHS,
+                self.model.fit(np.array(self.X), np.array(self.y), sample_weight=np.array(self.w), epochs=epochs,
                                verbose=0)
                 self.model.save('dino_2.model')
             self.first_game = False
 
     def live_play(self, game_over, environ, restart):
 
-        empty = [0] * 32
+        empty = [0] * self.env_len
         g_start = time.time()
 
         while self.alive:
@@ -112,7 +106,9 @@ class Pac:
 
                 if cur_env != empty and (time.time() - self.jump_time) > 0.35:
                     with self.graph.as_default():
-                        p = self.model.predict(np.array([self.mem[-1][32:-1] + cur_env]))[0][0]
+                        view = self.mem[-1][0][self.env_len:] + cur_env
+                        p = self.model.predict(np.array([view]))[0][0]
+
                         if self.first_game:
                             x = np.random.choice([0, 1], p=[0.5, 0.5])
                         else:
@@ -121,11 +117,4 @@ class Pac:
                         if x == 1:
                             self.dino_jump()
 
-                        self.mem.append(self.mem[-1][32:-1] + cur_env + [x])
-
-        #except (KeyboardInterrupt, SystemExit):
-        #    self.alive = False
-        #    print('exit')
-
-
-
+                        self.mem.append((view, x))
